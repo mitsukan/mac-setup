e2663191d3889b52dd4f19e61e00068e1561337b
mitsukan <jim_ip20@hotmail.com>
2022-02-24 14:50:25 +0000

    Adding actions step, uninstalling github action's built in homebrew

---
+++ tests/uninstall-homebrew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Uninstalls Homebrew using the official uninstall script.
+# Download and run the uninstall script.
+curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
+chmod +x ./uninstall.sh
+sudo ./uninstall.sh --force
+
+# Clean up Homebrew directories.
+sudo rm -rf /usr/local/bin/brew
+sudo rm -rf /usr/local/Homebrew
+sudo rm -rf /usr/local/Caskroom
